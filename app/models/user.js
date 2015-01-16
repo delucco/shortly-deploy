5ee214f1ce14ade163e7f213babb9abbcc2fd74a
Hack Reactor Students <accounts@hackreactor.com>
2015-01-15 20:25:50 -0800

    refactored models with mongoose

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,69 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var mongoose = require('mongoose');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function(){
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
+
+var userSchema = mongoose.Schema({
+  username: {type: String, index: {unique: true } },
+  password: String,
+  created_at: { type: Date, default: Date.now }
 });
 
+var User = mongoose.model('User', userSchema);
+
+// var hashify = function(){
+// }
+
+userSchema.pre('save', function(next) {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+    next();
+  });
+});
+
+User.prototype.comparePassword = function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+    callback(err, isMatch);
+  });
+};
+
+var user = new User({
+  username: 'omar',
+  password: 'omar'
+});
+
+// user.save(function(err){
+//   if (err) {console.error("there is an error");}
+//   User.find(function(err, users){
+//     if (err) {console.error("there is an error");}
+//     console.log("user equals: ", users);
+//     var query  = User.where({ username: 'omar' });
+//     query.findOne(function(err, user){
+//       if (err) {console.error('You fucked up');}
+//       user.comparePassword('omar', function(err, isMatch){
+//         if (err) {console.error('You REALLY fucked up');}
+//         console.log(isMatch);
+//       });
+//     });
+//   });
+// });
+
 module.exports = User;
+
+
+
+
+
+// var User = db.Model.extend({
+//   tableName: 'users',
+//   hasTimestamps: true,
+//   initialize: function(){
+//     this.on('creating', this.hashPassword);
+//   },
+//   hashPassword: function(){
+//   }
+// });
+
